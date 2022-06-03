c6bb678fa4126726b938b296449fc2e2928c9c80
kwak2864 <kwak2864@emoney.co.kr>
2022-06-03 11:13:52 +0900

    feat: todo-app develop
    todolist

---
+++ src/Components/TodoList.js
@@ -0,0 +1,15 @@
+import TodoListItem from './TodoListItem'
+import './TodoList.scss'
+
+function TodoList({ todos, onRemove, onToggle }) {
+  console.log(todos)
+  return (
+    <div className="TodoList">
+      {todos.map(todo => (
+        < TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
+      ))}
+    </div>
+  )
+}
+
+export default TodoList;
